378f2f3e755c9507ba275c46b7f54f3fd1572d61
okanucak <okanucak@hotmail.com>
2025-03-14 22:21:21 +0300

    Add files via upload

---
+++ lang.h
@@ -0,0 +1,76 @@
+struct messages
+{
+    char *YOUR_BALANCE;
+    char *YOUR_TARGET;
+    char *START_BALANCE;
+    char *ZERO_TEXT;
+    char *ODD_TEXT;
+    char *EVEN_TEXT;
+    char *YOUR_CHOICE;
+    char *BALANCE;
+    char *BET;
+    char *WAIT;
+    char *INCOMING_NUMBER;
+    char *LUCKY;
+    char *UNLUCKY;
+    char *CRITICAL;
+    char *SORRY;
+    char *YOU_WIN;
+    char *YOU_LOST;
+};
+
+struct messages trMessages;
+struct messages enMessages;
+
+void setMessages() {
+    trMessages.YOUR_BALANCE = "Bakiyeniz";
+    enMessages.YOUR_BALANCE = "Your Balance";
+
+    trMessages.YOUR_TARGET = "Hedefiniz";
+    enMessages.YOUR_TARGET = "Your Target";
+
+    trMessages.START_BALANCE = "Başlangıç";
+    enMessages.START_BALANCE = "Starting";
+
+    trMessages.ZERO_TEXT = "Sıfır";
+    enMessages.ZERO_TEXT = "Zero";
+
+    trMessages.ODD_TEXT = "Tek";
+    enMessages.ODD_TEXT = "Odd";
+
+    trMessages.EVEN_TEXT = "Çift";
+    enMessages.EVEN_TEXT = "Even";
+
+    trMessages.YOUR_CHOICE = "Seçiminiz";
+    enMessages.YOUR_CHOICE = "Your Choice";
+
+    trMessages.BALANCE = "Bakiye";
+    enMessages.BALANCE = "Balance";
+
+    trMessages.BET = "Bahis";
+    enMessages.BET = "Bet";
+
+    trMessages.WAIT = "Bekleyiniz...";
+    enMessages.WAIT = "Wait...";
+
+    trMessages.INCOMING_NUMBER = "Gelen sayı";
+    enMessages.INCOMING_NUMBER = "Incoming number";
+
+    trMessages.LUCKY = "İyisin :)";
+    enMessages.LUCKY = "You are good :)";
+
+    trMessages.UNLUCKY = "Sıkıntı yok beti arttırıyorum ;)";
+    enMessages.UNLUCKY = "No problem, I am increasing the bet ;)";
+
+    trMessages.CRITICAL = "Durum kritik :(";
+    enMessages.CRITICAL = "The situation is critical :(";
+
+    trMessages.SORRY = ":(";
+    enMessages.SORRY = ":(";
+
+    trMessages.YOU_WIN = "+++Kazandın+++";
+    enMessages.YOU_WIN = "+++You won+++";
+
+    trMessages.YOU_LOST = "---Kaybettin---";
+    enMessages.YOU_LOST = "---You lost---";
+}
--- main.c
+++ main.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 
-#include "lang/turkish.h"
+#include "lang.h"
 
 const int ZERO_VALUE = 0;
 const int ODD_VALUE = 1;
@@ -17,31 +18,41 @@ int choice;
 void initial();
 void run();
 void finitial();
-void inputAmount(int *amount, char message[255]);
+void inputAmount(int *amount, char *message);
 void inputChoice();
 void showAmounts();
 void spin();
 int getNumber();
 
+struct messages lang;
 
-int main() {
+
+int main(int argc, char *argv[]) {
+
+    srand(time(NULL));
+
+    setMessages();
+    lang = trMessages;
+    if (argc > 1) {
+        if (strcmp(argv[1], "en") == 0) {
+            lang = enMessages;
+        }
+    }
 
     initial();
     
     run();
     
     finitial();
-    
+
     return 0;
 }
 
 
 void initial() {
-    srand(time(NULL));
-
-    inputAmount(&balance, YOUR_BALANCE);
-    inputAmount(&target, YOUR_TARGET);
-    inputAmount(&startBet, START_BALANCE);
+    inputAmount(&balance, lang.YOUR_BALANCE);
+    inputAmount(&target, lang.YOUR_TARGET);
+    inputAmount(&startBet, lang.START_BALANCE);
 
     if (startBet > balance) {
         startBet = balance;
@@ -63,44 +74,44 @@ void finitial() {
     showAmounts();
 
     if (balance >= target) {
-        puts(YOU_WIN);
+        puts(lang.YOU_WIN);
     } else {
-        puts(YOU_LOST);
+        puts(lang.YOU_LOST);
     }
 }
 
 void spin() {
     int number = getNumber();
 
-    printf("\n%s\n\n", WAIT);
+    printf("\n%s\n\n", lang.WAIT);
 
     for (int i = 1; i <= 2000000000; i++);
 
     if (number != ZERO_VALUE) {
-        printf("%s:\t%d\n", INCOMING_NUMBER, number);
+        printf("%s:\t%d\n", lang.INCOMING_NUMBER, number);
     } else {
-        printf("%s:\t%s\n", INCOMING_NUMBER, ZERO_TEXT);
+        printf("%s:\t%s\n", lang.INCOMING_NUMBER, lang.ZERO_TEXT);
     }
 
     if (number != ZERO_VALUE && number % 2 == choice % 2) {
         balance += bet;
         bet = startBet;
-        puts(LUCKY);
+        puts(lang.LUCKY);
     } else {
         balance -= bet;
         bet *= 2;
         if (balance > bet) {
-            puts(UNLUCKY);
+            puts(lang.UNLUCKY);
         } else if (balance > 0) {
             bet = balance;
-            puts(CRITICAL);
+            puts(lang.CRITICAL);
         } else {
-            puts(SORRY);
+            puts(lang.SORRY);
         }
     }
 }
 
-void inputAmount(int *amount, char message[255]) {
+void inputAmount(int *amount, char *message) {
     char input[10];
 
     do {
@@ -111,14 +122,14 @@ void inputAmount(int *amount, char message[255]) {
 }
 
 void showAmounts() {
-    printf("\n%s\t:\t%d\n%s\t:\t%d\n", BALANCE, balance, BET, bet);
+    printf("\n%s\t:\t%d\n%s\t:\t%d\n", lang.BALANCE, balance, lang.BET, bet);
 }
 
 void inputChoice() {
     char input[10];
 
     do {
-        printf("\n%s\t\t(%d)\n%s\t\t(%d)\n%s\t:", ODD, ODD_VALUE, EVEN, EVEN_VALUE, YOUR_CHOICE);
+        printf("\n%s\t\t(%d)\n%s\t\t(%d)\n%s\t:", lang.ODD_TEXT, ODD_VALUE, lang.EVEN_TEXT, EVEN_VALUE, lang.YOUR_CHOICE);
         scanf("%s", input);
         choice = atoi(input);
     } while (choice != ODD_VALUE && choice != EVEN_VALUE);
@@ -127,4 +138,3 @@ void inputChoice() {
 int getNumber() {
     return rand() % 37;
 }
-
